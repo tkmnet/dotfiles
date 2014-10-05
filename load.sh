12bb47283db60c530330924663df5ce19c429796
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-10-06 01:29:51 +0900

    Save

--- load.sh
+++ load.sh
@@ -5,3 +5,5 @@ git pull
 cp ./vim/.vimrc ~/
 cp -r ./vim/.vim/ ~/
 mkdir ~/.vim_temp
+
+cp ./tcsh/.tcshrc ~/
--- save.sh
+++ save.sh
@@ -3,6 +3,8 @@
 cp ~/.vimrc ./vim/
 cp -r ~/.vim/ ./vim/
 
+cp ~/.tcshrc ./tcsh/
+
 git add -f *
 git commit -am "Save"
 git push
