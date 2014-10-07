89323d8ade41c4d01a4ebf0b59010c8f7386e878
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-10-08 02:46:55 +0900

    Save

--- load.sh
+++ load.sh
@@ -5,7 +5,9 @@ cd `dirname $0`
 git pull
 
 cp ./vim/.vimrc ~/
-cp -r ./vim/.vim/ ~/
-mkdir ~/.vim_temp
+cp -r ./vim/.vim/ ~/ 2>/dev/null
+mkdir ~/.vim_temp 2>/dev/null
 
-cp ./tcsh/.* ~/
+cp ./tcsh/.tcshrc ~/
+cp ./tcsh/.login ~/
+cp ./tcsh/.logout ~/
