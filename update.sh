d550518bdec713ff7a861508c696319409967e20
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-10-28 05:15:45 +0900

    Save

--- update.sh
+++ update.sh
@@ -9,13 +9,13 @@ ln -sf ./vim/.vimrc ~/
 mkdir ~/.vim_temp 2>/dev/null
 ln -sf ./vim/.vimrc ~/
 mkdir ~/.vim/userautoload 2>/dev/null
-ln -sf ./vim/.vim/userautoload/ ~/.vim/
+ln -sf ./vim/.vim/userautoload/* ~/.vim/
 mkdir ~/.vim/colors 2>/dev/null
-ln -sf ./vim/.vim/colors/ ~/.vim/
+ln -sf ./vim/.vim/colors/* ~/.vim/
 mkdir ~/.vim/snippets 2>/dev/null
-ln -sf ./vim/.vim/snippets/ ~/.vim/
+ln -sf ./vim/.vim/snippets/* ~/.vim/
 mkdir ~/.vim/bundle/neobundle.vim 2>/dev/null
-ln -sf ./vim/.vim/bundle/neobundle.vim/ ~/.vim/bundle/
+ln -sf ./vim/.vim/bundle/neobundle.vim/* ~/.vim/bundle/
 
 ln -sf ./tcsh/.tcshrc ~/
 ln -sf ./tcsh/.login ~/
