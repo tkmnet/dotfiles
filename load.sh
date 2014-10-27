689bd04be8d88ad23cebffd0c9e51e37ab736cc5
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-10-28 05:11:53 +0900

    Save

--- load.sh
+++ load.sh
@@ -4,15 +4,17 @@ cd `dirname $0`
 
 git pull
 
+ln -sf ./vim/.vimrc ~/
 mkdir ~/.vim_temp 2>/dev/null
 ln -sf ./vim/.vimrc ~/
-mkdir ~/.vim 2>/dev/null
-ln -sf ./vim/.vim/userautoload ~/.vim/
-ln -sf ./vim/.vim/colors ~/.vim/
-ln -sf ./vim/.vim/snippets ~/.vim/
-ln -sf ./vim/.vimrc ~/
-mkdir ~/.vim/bundle 2>/dev/null
-ln -sf ./vim/.vim/bundle/neobundle.vim ~/.vim/bundle/
+mkdir ~/.vim/userautoload 2>/dev/null
+ln -sf ./vim/.vim/userautoload/ ~/.vim/
+mkdir ~/.vim/colors 2>/dev/null
+ln -sf ./vim/.vim/colors/ ~/.vim/
+mkdir ~/.vim/snippets 2>/dev/null
+ln -sf ./vim/.vim/snippets/ ~/.vim/
+mkdir ~/.vim/bundle/neobundle.vim 2>/dev/null
+ln -sf ./vim/.vim/bundle/neobundle.vim/ ~/.vim/bundle/
 
 ln -sf ./tcsh/.tcshrc ~/
 ln -sf ./tcsh/.login ~/
