2dbbfc1259f30c322fa954719f6fc53673258f00
Shunki <tkmnet@users.noreply.github.com>
2016-02-22 18:08:42 +0900

    Update xmonad.hs

--- UNIQUE/moogle/xmonad/.xmonad/xmonad.hs
+++ UNIQUE/moogle/xmonad/.xmonad/xmonad.hs
@@ -79,7 +79,8 @@ main = do
        , layoutHook = toggleLayouts (noBorders Full) $ avoidStruts $ myLayout
         -- xmobar setting
        , logHook = myLogHook wsbar
-       , handleEventHook = fadeWindowsEventHook
+       , handleEventHook = fadeWindowsEventHook <+>
+                           docksEventHook
        , workspaces = myWorkspaces
        , modMask = modm }
 
