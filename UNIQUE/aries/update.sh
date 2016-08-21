7a5c56def0d693d7dda1ccd1878c0bc12f250e83
Shunki <tkmnet@users.noreply.github.com>
2016-08-22 05:51:26 +0900

    Update update.sh

--- UNIQUE/aries/update.sh
+++ UNIQUE/aries/update.sh
@@ -5,7 +5,7 @@ ln -sf $2 $1/xmonad/.xmonad/autostart ~/.xmonad/
 ln -sf $1/xmonad/.xmonad/xmonad.hs ~/.xmonad/
 ln -sf $1/xmonad/.xmonad/xmobarrc ~/.xmonad/
 ln -sf $1/xmonad/.xmonad/autostart.sh ~/.xmonad/
-ln -sf $1/xmonad/.xmonad/fcitx.sh ~/.xmonad/
+ln -sf $1/xmonad/.xmonad/uim.sh ~/.xmonad/
 ln -sf $1/xmonad/.xmonad/airplane.sh ~/.xmonad/
 ln -sf $1/xmonad/.xmonad/celler.sh ~/.xmonad/
 ln -sf $1/xmonad/.xmonad/wlipaddr.sh ~/.xmonad/
