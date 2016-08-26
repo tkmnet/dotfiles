7664695dd7b980de160f18685ec2762f2d26f448
Shunki Takami <tkmnet@users.noreply.github.com>
2016-08-26 22:09:54 +0900

    update

--- UNIQUE/aries/xmonad/.xmonad/xmonad.hs
+++ UNIQUE/aries/xmonad/.xmonad/xmonad.hs
@@ -1,14 +1,5 @@
-import qualified Data.Map as M
-
 import XMonad
 import qualified XMonad.StackSet as W  -- myManageHookShift
-
-import Control.Monad (liftM2)          -- myManageHookShift
-import System.IO                       -- for xmobar
-import System.Cmd
-
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Actions.WindowGo
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.DynamicLog         -- for xmobar
 import XMonad.Hooks.EwmhDesktops
@@ -16,31 +7,18 @@ import XMonad.Hooks.FadeWindows
 import XMonad.Hooks.ManageDocks        -- avoid xmobar area
 import XMonad.Hooks.Place
 import XMonad.Hooks.SetWMName
-import XMonad.Layout
-import XMonad.Layout.DragPane          -- see only two window
-import XMonad.Layout.Gaps
-import XMonad.Layout.IM
-import XMonad.Layout.Magnifier         -- this makes window bigger
-import XMonad.Layout.MultiToggle
-import XMonad.Layout.MultiToggle.Instances
-import XMonad.Layout.Named
 import XMonad.Layout.NoBorders         -- In Full mode, border is no use
 import XMonad.Layout.ResizableTile     -- Resizable Horizontal border
 import XMonad.Layout.Spacing           -- this makes smart space around windows
-import XMonad.Layout.Tabbed
 import XMonad.Layout.Grid
-import XMonad.Layout.ThreeColumns      -- for many windows
 import XMonad.Layout.ToggleLayouts     -- Full window at any time
 import XMonad.Util.EZConfig            -- removeKeys, additionalKeys
-import XMonad.Util.Run(spawnPipe)      -- spawnPipe, hPutStrLn
 import XMonad.Util.Run
-import Graphics.X11.ExtraTypes.XF86
-
 
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
 modm = mod4Mask
 altMask = mod1Mask
-
 colorThink     = "#E2231A"
 colorBlue      = "#857da9"
 colorBlue2     = "#1e90ff"
@@ -53,121 +31,88 @@ colorNormalbg  = "#1a1e1b"
 
 myTerminal = "xfce4-terminal"
 
--- Define keys to remove
-keysToRemove x =
-    [
-          (modm .|. shiftMask, xK_Return)
-        -- Unused gmrun binding
-        , (modm .|. shiftMask, xK_p)
-        -- Unused close window binding
-        , (modm .|. shiftMask, xK_c)
-    ]
-
--- Delete the keys combinations we want to remove.
-strippedKeys x = foldr M.delete (keys defaultConfig x) (keysToRemove x)
-
 main :: IO ()
 
 main = do
-    wsbar <- spawnPipe myWsBar
+    wsbar <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
     xmonad $ ewmh defaultConfig
        { borderWidth = 2
        , terminal = myTerminal
        , normalBorderColor = colorGray
        , focusedBorderColor = colorThink
        , startupHook = myStartupHook
-       , manageHook = placeHook myPlacement <+> 
---                      myManageHookShift <+>
+       , manageHook = placeHook simpleSmart <+> 
                       myManageHookFloat <+>
                       manageDocks
-        -- any time Full mode, avoid xmobar area
-       , layoutHook = toggleLayouts (noBorders Full) $ avoidStruts $ myLayout
-        -- xmobar setting
+       , layoutHook = toggleLayouts Full $ avoidStruts $ myLayout
        , logHook = myLogHook wsbar
        , handleEventHook = fadeWindowsEventHook <+>
                            docksEventHook
        , workspaces = myWorkspaces
        , modMask = modm }
 
+       `removeKeys`
+        [
+          (modm              , xK_Return)
+        , (modm .|. shiftMask, xK_Return)
+        , (modm              , xK_p     )
+        , (modm .|. shiftMask, xK_p     )
+        , (modm .|. shiftMask, xK_c     )
+        , (modm              , xK_n     )
+        , (modm              , xK_Tab   )
+        , (modm .|. shiftMask, xK_Tab   )
+        ]
+
        `additionalKeys`
        [ ((modm              , xK_Right ), nextWS ) -- go to next workspace
        , ((modm              , xK_Left  ), prevWS ) -- go to prev workspace
-       --, ((modm              , xK_v     ), sendMessage ToggleLayout)
-       --, ((modm              , xK_g     ), sendMessage ToggleGaps)
-       , ((modm .|. shiftMask, xK_l     ), sendMessage MirrorExpand)
-       , ((modm .|. shiftMask, xK_h     ), sendMessage MirrorShrink)
+       , ((modm              , xK_h     ), sendMessage MirrorExpand)
+       , ((modm              , xK_l     ), sendMessage MirrorShrink)
        , ((modm .|. shiftMask, xK_Right ), shiftToNext)
        , ((modm .|. shiftMask, xK_Left  ), shiftToPrev)
-       --, ((modm              , xK_comma ), sendMessage MagnifyLess)   -- smaller window
-       --, ((modm              , xK_period), sendMessage MagnifyMore)  -- bigger window
        , ((modm              , xK_j     ), windows W.focusDown)
        , ((modm              , xK_k     ), windows W.focusUp)
        , ((modm .|. shiftMask, xK_j     ), windows W.swapDown)
        , ((modm .|. shiftMask, xK_k     ), windows W.swapUp) ]
+       
+       `additionalKeys`
+       [ ((modm .|. s, k), windows $ f i)
+        | (i, k) <- zip myWorkspaces ([xK_1 .. xK_9] ++ [xK_0])
+        , (f, s) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+       ]
+
+       `additionalKeys`
+       [ ((modm                    , xK_c      ), kill) -- %! Close the focused window
+       ]
+
+       `additionalKeys`
+       [ ((modm              , xK_Return ), spawn myTerminal)
+       , ((modm              , xK_p      ), spawn "import -window root ~/Pictures/Screenshot/`date +%y%m%d_%H%M%S`.png")
+       , ((modm .|. shiftMask, xK_p      ), spawn "import -frame ~/Pictures/Screenshot/`date +%y%m%d_%H%M%S`.png")
+       , ((modm              , xK_w      ), spawn "firefox")
+       , ((modm              , xK_s      ), spawn "firefox --new-window 'https://www.google.co.jp/'")
+       , ((modm              , xK_f      ), spawn "pcmanfm")
+       , ((modm              , xK_o      ), spawn "gksudo poweroff")
+       , ((modm              , xK_F5     ), spawn "xbacklight - 10")
+       , ((modm              , xK_F6     ), spawn "xbacklight + 10")
+       , ((modm              , xK_Escape ), spawn "systemctl suspend")
+       , ((modm              , xK_r      ), spawn "xfrun4 -q;xfrun4 -r")
+       , ((modm .|. altMask  , xK_l      ), spawn "dm-tool lock")
+       ]
 
        `additionalKeys`
        [ ((modm .|.   altMask, xK_d     ), spawn "XMODIFIERS=@im=uim /usr/lib/mozc/mozc_tool --mode=word_register_dialog")
        , ((modm .|.   altMask, xK_r     ), spawn "uim-remote -r") ]
-       
-       `additionalKeys`
-        [ ((m .|. modm, k), windows $ f i)
-         | (i, k) <- zip myWorkspaces ([xK_1 .. xK_9] ++ [xK_0])
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-        ]
---       [ ((modm .|. m, k), windows $ f i)
---         | (i, k) <- zip myWorkspaces
---                     [ xK_exclam, xK_at, xK_numbersign
---                     , xK_dollar, xK_percent, xK_asciicircum
---                     , xK_ampersand, xK_asterisk, xK_parenleft
---                     , xK_parenright
---                     ]
---       , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
---       ]
 
-       `additionalKeys`
-       [ ((modm                    , xK_Return ), spawn myTerminal)
-       , ((modm                    , xK_c      ), kill) -- %! Close the focused window
-       , ((modm                    , xK_p  ), spawn "import -window root ~/Pictures/Screenshot/`date +%y%m%d_%H%M%S`.png")
-       , ((modm .|. shiftMask      , xK_p  ), spawn "import -frame ~/Pictures/Screenshot/`date +%y%m%d_%H%M%S`.png")
-       , ((modm                    , xK_w ), spawn "firefox")
-       , ((modm                    , xK_s ), spawn "firefox --new-window 'https://www.google.co.jp/'")
-       , ((modm                    , xK_f ), spawn "pcmanfm")
-       , ((modm                    , xK_o ), spawn "gksudo poweroff")
-       , ((modm                    , xK_F5 ), spawn "xbacklight - 10")
-       , ((modm                    , xK_F6 ), spawn "xbacklight + 10")
-       , ((modm                    , xK_F8 ), spawn "gksudo \"wpa_cli -i wlp3s0 enable_network all\"")
-       , ((modm .|. shiftMask      , xK_F8 ), spawn "gksudo \"wpa_gui -i wlp3s0\"")
-       , ((modm                    , xK_Escape ), spawn "systemctl suspend")
-       , ((modm                    , xK_r ), spawn "xfrun4 -q;xfrun4 -r")
-       , ((modm                    , xK_l ), spawn "lock")
-       ]
-
--- Handle Window behaveior
 myLayout = (spacing 5 $ (Grid))
        ||| (spacing 5 $ (ResizableTall 1 (3/100) (3/5) []))
        ||| (spacing 5 $ (Mirror $ (ResizableTall 1 (3/100) (3/5) [])))
---       ||| (spacing 5 $ (dragPane Vertical   (1/10) (1/2)))
---       ||| (spacing 5 $ (dragPane Horizontal (1/10) (1/2)))
-       ||| (spacing 5 $ (Full))
+       ||| (noBorders Full)
 
-
-
--- Start up (at xmonad beggining), like "wallpaper" or so on
 myStartupHook = do
         setWMName "LG3D"
-        spawn "gnome-settings-daemon"
-        spawn "gnome-sound-applet"
         spawn "./.xmonad/autostart.sh"
 
--- some window must created there
---myManageHookShift = composeAll
-            -- if you want to know className, type "$ xprop|grep CLASS" on shell
---            [--className =? "Chromium"   --> doShift "main"
---             className =? "Mplayer"   --> doShift "media"
---            ]
---            where viewShift = doF . liftM2 (.) W.view W.shift
-
--- new window will created in Float mode
 myManageHookFloat = composeAll
             [ className =? "MPlayer"           --> doFloat,
               className =? "Gimp"              --> doFloat,
@@ -178,10 +123,7 @@ myManageHookFloat = composeAll
               title     =? "Execute File"      --> doFloat,
               title     =? "Speedbar"          --> doFloat]
 
-
 myLogHook h = dynamicLogWithPP $ wsPP { ppOutput = hPutStrLn h }
-
-myWsBar = "xmobar ~/.xmonad/xmobarrc"
 
 wsPP = xmobarPP { ppOrder               = \(ws:l:t:_)   -> [ws,t]
                 , ppCurrent             = xmobarColor   colorThink       colorNormalbg
@@ -193,7 +135,4 @@ wsPP = xmobarPP { ppOrder               = \(ws:l:t:_)   -> [ws,t]
                 , ppWsSep               = " "
                 , ppSep                 = " :: "
                 }
-
-
-myPlacement = fixed (0.5, 0.5)
 
