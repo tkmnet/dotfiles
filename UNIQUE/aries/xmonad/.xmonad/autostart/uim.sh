386f6e47358852a34bf3be64e31ff2116f2e07f0
Shunki <tkmnet@users.noreply.github.com>
2016-08-28 01:53:08 +0900

    Update uim.sh

--- UNIQUE/aries/xmonad/.xmonad/autostart/uim.sh
+++ UNIQUE/aries/xmonad/.xmonad/autostart/uim.sh
@@ -1,1 +1,4 @@
-uim-xim
+/usr/bin/systemctl --user set-environment XMODIFIERS=@im=uim
+/usr/bin/systemctl --user set-environment GTK_IM_MODULE=uim
+/usr/bin/systemctl --user set-environment QT_IM_MODULE=uim
+/usr/bin/uim-xim
