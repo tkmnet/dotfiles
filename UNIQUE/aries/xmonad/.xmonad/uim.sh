4b44aebf2ed633a55a4af3bbcdd74dcf958324f2
Shunki <tkmnet@users.noreply.github.com>
2016-09-12 17:03:12 +0900

    Update uim.sh

--- UNIQUE/aries/xmonad/.xmonad/uim.sh
+++ UNIQUE/aries/xmonad/.xmonad/uim.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-case `uim-remote` in
-	1)
-		echo -n '<fc=#ffffff> E </fc>'
-		;;
-	2)
-		echo -n '<fc=#ffffff,#ff0000> J </fc>'
-		;;
+if test `ps aux | grep uim-xim | wc -l` -lt 2; then
+	echo -n ' - '
+	exit 0
+fi
+
+case "`/usr/local/bin/uim-remote`" in
+	"1")
+		echo -n '<fc=#ffffff> E </fc>' ;;
+	"2")
+		echo -n '<fc=#ffffff,#ff0000> J </fc>' ;;
 	*)
-		echo -n ' - '
+		echo -n ' - ' ;;
 esac
+
+exit 0
