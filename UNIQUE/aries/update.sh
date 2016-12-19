c0527af57f48400eec392af83f6b5078451b9035
Shunki Takami <tkmnet@users.noreply.github.com>
2016-12-19 13:31:29 +0900

    update

--- UNIQUE/aries/update.sh
+++ UNIQUE/aries/update.sh
@@ -14,5 +14,6 @@ ln -sf $1/xmonad/.xmonad/battery.sh ~/.xmonad/
 mkdir -p ~/.config/openbox 2>/dev/null
 ln -sf $1/lxde/openbox/lxde-rc.xml ~/.config/openbox/
 mkdir -p ~/.config/autostart/resource 2>/dev/null
+ln -sf $1/lxde/autostart/autostart-script.desktop ~/.config/autostart/
 ln -sf $1/lxde/autostart/resource/autostart.sh ~/.config/autostart/resource/
 ln -sf $2 $1/lxde/autostart/resource/autostart ~/.config/autostart/resource/
